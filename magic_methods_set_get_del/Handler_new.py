4825864a3d2ce8e3878af9a09e16dd117dca9409
Danila Beskrokov <beskrokovd@mail.ru>
2024-11-10 16:30:35 +0300

    added Handler

---
+++ magic_methods_set_get_del/Handler_new.py
@@ -0,0 +1,43 @@
+class Handler:
+    def __init__(self, methods=('GET', )):
+        self.__methods = methods
+
+
+
+    def __call__(self, func, *args, **kwargs):
+        def wrapper(request):
+            m = request.get('method', 'GET')
+            if m in self.__methods:
+                method = m.lower()
+                return self.__getattribute__(method)(func, request)
+
+        return wrapper
+
+    def get(self, func, request, *args, **kwargs):
+        return f"GET: {func(request)}"
+
+    def post(self, func, request, *args, **kwargs):
+        return f"POST: {func(request)}"
+
+
+
+assert hasattr(Handler, 'get') and hasattr(Handler, 'post'), "класс Handler должен содержать методы get и post"
+
+@Handler(methods=('GET', 'POST'))
+def contact2(request):
+    return "контакты"
+
+# Это равно obj = Handler(contact2)
+
+assert contact2({"method": "POST"}) == "POST: контакты", "декорированная функция вернула неверные данные"
+assert contact2({"method": "GET"}) == "GET: контакты", "декорированная функция вернула неверные данные"
+# assert contact2({"method": "DELETE"}) is None, "декорированная функция вернула неверные данные"
+# assert contact2({}) == "GET: контакты", "декорированная функция вернула неверные данные при указании пустого словаря"
+#
+# @Handler(methods=('POST'))
+# def index(request):
+#     return "index"
+#
+# assert index({"method": "POST"}) == "POST: index", "декорированная функция вернула неверные данные"
+# assert index({"method": "GET"}) is None, "декорированная функция вернула неверные данные"
+# assert index({"method": "DELETE"}) is None, "декорированная функция вернула неверные данные"
